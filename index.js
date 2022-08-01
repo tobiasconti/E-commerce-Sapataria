78ebffbfdbdba6fb4957c4216efc0061e705631e
Tobias Conti <102184278+tobiasconti@users.noreply.github.com>
2022-08-01 18:53:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const $contactButton = document.querySelector('.contact-button')
+const $contactBox = document.querySelectorAll('.contact')
+
+$contactButton.addEventListener('click', function(){
+    scrollBy(1500, 900)
+})
