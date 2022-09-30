c53145d704d375811600ffefd0f0555e682e3a85
Tobias <tobias.conti@webhit.dev>
2022-09-30 17:00:36 -0300

    Ajustes no layou

--- index.js
+++
@@ -1,6 +0,0 @@
-const $contactButton = document.querySelector('.contact-button')
-const $contactBox = document.querySelectorAll('.contact')
-
-$contactButton.addEventListener('click', function(){
-    scrollBy(1500, 900)
-})
